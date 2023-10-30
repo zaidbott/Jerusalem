b8658f2ca2e6a7cb8b5de594dbe153613a9894ce
روجر <104784913+rogerpq@users.noreply.github.com>
2023-10-30 20:20:24 +0300

    Update managers.py

--- repthon/core/managers.py
+++ repthon/core/managers.py
@@ -78,7 +78,7 @@ async def edit_delete(event, text, time=None, parse_mode=None, link_preview=None
             )
         )
     else:
-        zedevent = await event.edit(
+        repevent = await event.edit(
             text, link_preview=link_preview, parse_mode=parse_mode
         )
     await asyncio.sleep(time)
