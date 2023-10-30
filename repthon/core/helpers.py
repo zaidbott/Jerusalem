e71e9ca2204ef9cb7aa7e8b3a65ee0985a8a4af9
روجر <104784913+rogerpq@users.noreply.github.com>
2023-10-30 20:10:59 +0300

    Update helpers.py

--- repthon/core/helpers.py
+++ repthon/core/helpers.py
@@ -6,7 +6,7 @@ from telethon.utils import get_display_name
 
 from .events import NewMessage
 
-LOGGER = logging.getLogger("zthon")
+LOGGER = logging.getLogger("𝗥𝗲𝗽𝘁𝗵𝗼𝗻")
 
 
 def printUser(entity: types.User) -> None:
