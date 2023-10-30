6f07922c2386e35376553c5cc713643a93440b8f
روجر <104784913+rogerpq@users.noreply.github.com>
2023-10-30 20:36:46 +0300

    Update paste.py

--- repthon/helpers/utils/paste.py
+++ repthon/helpers/utils/paste.py
@@ -31,9 +31,9 @@ async def p_paste(message, extension=None):
             else f"https://pasty.lus.pm/{response['id']}.txt"
         )
         try:
-            from ...core.session import zedub
+            from ...core.session import zq_lo
 
-            await zedub.send_message(
+            await zq_lo.send_message(
                 Config.BOTLOG_CHATID,
                 f"**You have created a new paste in pasty bin.** Link to pasty is [here]({purl}). You can delete that paste by using this token `{response['deletionToken']}`",
             )
