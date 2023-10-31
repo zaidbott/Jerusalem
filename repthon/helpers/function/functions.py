337cc04942bfe20a399204e0f28e8dd69e0357fd
روجر <104784913+rogerpq@users.noreply.github.com>
2023-10-31 13:05:56 +0300

    Update functions.py

--- repthon/helpers/function/functions.py
+++ repthon/helpers/function/functions.py
@@ -98,8 +98,8 @@ async def covidindia(state):
 async def post_to_telegraph(
     page_title,
     html_format_content,
-    auth_name="Zed-Thon",
-    auth_url="https://t.me/ZThon",
+    auth_name="Repthon",
+    auth_url="https://t.me/Repthon",
 ):
     post_client = TelegraphPoster(use_api=True)
     post_client.create_api_token(auth_name)
