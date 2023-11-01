5800205148e1aa933dfc4dd905f417fe42a36be6
روجر <104784913+rogerpq@users.noreply.github.com>
2023-11-01 20:45:10 +0300

    Update الاذاعه.py

--- repthon/plugins/الاذاعه.py
+++ repthon/plugins/الاذاعه.py
@@ -1,6 +1,6 @@
 from telethon import events
 
-from zthon import zedub
+from repthon import zq_lo
 
 from ..core.managers import edit_or_reply
 from . import BOTLOG, BOTLOG_CHATID
@@ -8,18 +8,18 @@ from . import BOTLOG, BOTLOG_CHATID
 plugin_category = "البوت"
 from . import * 
 
-ZED_BLACKLIST = [
+REP_BLACKLIST = [
     -1001236815136,
     -1001614012587,
     ]
 
-ZED_BBLACKLIST = [
+REP_BBLACKLIST = [
     777000,
     ]
 #
 
-ZelzalPRO_cmd = (
-    "𓆩 [𝗦𝗼𝘂𝗿𝗰𝗲 𝑹𝑬𝑷𝑻𝑯𝑶𝑵🜑 𝗖𝗼𝗻𝗳𝗶𝗴 - اوامـر الاذا؏ـــة](t.me/Repthon) 𓆪\n\n"
+Baqir_cmd = (
+    "𓆩 [𝗦𝗼𝘂𝗿𝗰𝗲 𝗥𝗲𝗽𝘁𝗵𝗼𝗻 𝗖𝗼𝗻𝗳𝗶𝗴 - اوامـر الاذا؏ـــة](t.me/Repthon) 𓆪\n\n"
     "**⎞𝟏⎝** `.للمجموعات`\n"
     "**بالـرد ؏ــلى رسـالة او وسائـط او كتابـة رسـالة مع الامـࢪ**\n"
     "**- لـ اذاعـة رسـالة او ميديـا لكـل المجموعـات اللي انت موجود فيهـا . .**\n\n\n"
@@ -29,21 +29,21 @@ ZelzalPRO_cmd = (
     "**⎞𝟑⎝** `.خاص`\n"
     "**الامـر + معرف الشخص + الرسـاله . .**\n"
     " **- ارسـال رسـاله الى الشخص المحدد بدون الدخول للخاص وقراءة الرسـائل . .**\n\n"
-    "\n 𓆩 [𝙎𝙊𝙐𝙍𝘾𝞝 𝑹𝑬𝑷𝑻𝑯𝑶𝑵🜑](t.me/Repthon) 𓆪"
+    "\n 𓆩 [𝙎𝙊𝙐𝙍𝘾𝞝 𝙍𝙀𝙋𝙏𝙃𝙊𝙉](t.me/Repthon) 𓆪"
 )
 
 
 # Copyright (C) 2022 Zed-Thon . All Rights Reserved
-@zedub.zed_cmd(pattern="الاذاعه")
-async def cmd(zelzallll):
-    await edit_or_reply(zelzallll, ZelzalPRO_cmd)
+@zq_lo.rep_cmd(pattern="الاذاعه")
+async def cmd(baqir):
+    await edit_or_reply(baqir, Baqir_cmd)
 
 
-@zedub.zed_cmd(pattern=f"للمجموعات(?: |$)(.*)")
+@zq_lo.rep_cmd(pattern=f"للمجموعات(?: |$)(.*)")
 async def gcast(event):
-    zedthon = event.pattern_match.group(1)
-    if zedthon:
-        msg = zedthon
+    repthon = event.pattern_match.group(1)
+    if repthon:
+        msg = repthon
     elif event.is_reply:
         msg = await event.get_reply_message()
     else:
@@ -56,7 +56,7 @@ async def gcast(event):
         if x.is_group:
             chat = x.id
             try:
-                if chat not in ZED_BLACKLIST:
+                if chat not in REP_BLACKLIST:
                     await event.client.send_message(chat, msg)
                     done += 1
             except BaseException:
@@ -65,11 +65,11 @@ async def gcast(event):
         f"**✾╎تمت الاذاعـه بنجـاح الـى ** `{done}` **من المجموعـات ، خطـأ في الارسـال الـى ** `{er}` **من المجموعـات**"
     )
     
-@zedub.zed_cmd(pattern=f"للخاص(?: |$)(.*)")
+@zq_lo.rep_cmd(pattern=f"للخاص(?: |$)(.*)")
 async def gucast(event):
-    zedthon = event.pattern_match.group(1)
-    if zedthon:
-        msg = zedthon
+    repthon = event.pattern_match.group(1)
+    if repthon:
+        msg = repthon
     elif event.is_reply:
         msg = await event.get_reply_message()
     else:
@@ -82,7 +82,7 @@ async def gucast(event):
         if x.is_user and not x.entity.bot:
             chat = x.id
             try:
-                if chat not in ZED_BBLACKLIST:
+                if chat not in REP_BBLACKLIST:
                     done += 1
                     await event.client.send_message(chat, msg)
             except BaseException:
@@ -92,7 +92,7 @@ async def gucast(event):
     )
     
 
-@zedub.zed_cmd(pattern="خاص ?(.*)")
+@zq_lo.rep_cmd(pattern="خاص ?(.*)")
 async def pmto(event):
     r = event.pattern_match.group(1)
     p = r.split(" ")
@@ -107,7 +107,7 @@ async def pmto(event):
     if msg == "":
         return
     try:
-        await zedub.send_message(chat_id, msg)
+        await zq_lo.send_message(chat_id, msg)
         await event.edit("**✾╎تـم ارسال الرسـالة الـى الشخـص بـدون الدخـول للخـاص .. بنجـاح ✓**")
     except BaseException:
         await event.edit("**✾╎اووبس .. لقـد حدث خطـأ مـا .. اعـد المحـاوله**")
