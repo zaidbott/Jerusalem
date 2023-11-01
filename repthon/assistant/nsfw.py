c2b40d0cc66f4b215a0ced6cbaf403910b513556
روجر <104784913+rogerpq@users.noreply.github.com>
2023-11-01 14:14:29 +0300

    Update nsfw.py

--- repthon/assistant/nsfw.py
+++ repthon/assistant/nsfw.py
@@ -4,7 +4,7 @@ from telethon import Button
 from telethon.errors import MessageNotModifiedError
 from telethon.events import CallbackQuery
 
-from zthon import zedub
+from repthon import zq_lo
 
 from ..Config import Config
 from ..core.logger import logging
@@ -12,7 +12,7 @@ from ..core.logger import logging
 LOGS = logging.getLogger(__name__)
 
 
-@zedub.tgbot.on(CallbackQuery(data=re.compile(r"^age_verification_true")))
+@zq_lo.tgbot.on(CallbackQuery(data=re.compile(r"^age_verification_true")))
 async def age_verification_true(event: CallbackQuery):
     u_id = event.query.user_id
     if u_id != Config.OWNER_ID and u_id not in Config.SUDO_USERS:
@@ -37,7 +37,7 @@ async def age_verification_true(event: CallbackQuery):
         pass
 
 
-@zedub.tgbot.on(CallbackQuery(data=re.compile(r"^age_verification_false")))
+@zq_lo.tgbot.on(CallbackQuery(data=re.compile(r"^age_verification_false")))
 async def age_verification_false(event: CallbackQuery):
     u_id = event.query.user_id
     if u_id != Config.OWNER_ID and u_id not in Config.SUDO_USERS:
@@ -62,7 +62,7 @@ async def age_verification_false(event: CallbackQuery):
         pass
 
 
-@zedub.tgbot.on(CallbackQuery(data=re.compile(r"^chg_of_decision_")))
+@zq_lo.tgbot.on(CallbackQuery(data=re.compile(r"^chg_of_decision_")))
 async def chg_of_decision_(event: CallbackQuery):
     u_id = event.query.user_id
     if u_id != Config.OWNER_ID and u_id not in Config.SUDO_USERS:
