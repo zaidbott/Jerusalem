04f2d02b70bda3033ac44e6469fd9d5627655778
روجر <104784913+rogerpq@users.noreply.github.com>
2023-11-05 17:16:28 +0300

    Update ميمز.py

--- repthon/plugins/ميمز.py
+++ repthon/plugins/ميمز.py
@@ -456,3 +456,15 @@ async def jepmeme(memejep):
   url = f"https://t.me/MemeSoundJep/102"
   await memejep.client.send_file(memejep.chat_id,url,caption="",parse_mode="html",reply_to=Rep)
   await memejep.delete()
+@zq_lo.on(admin_cmd(outgoing=True, pattern="ولك حشاش$"))
+async def repmeme(memerep)
+ Rep = await reply_id(memerep)
+ url = f"https://t.me/Repthon_meme/3"
+ await memerep.client.send_file(memerep.chat_id,url,caption="",parse_mode="html",reply_to=Rep)
+ await memerep.delete()
+@zq_lo.on(admin_cmd(outgoing=True, pattern="انت شرايود$"))
+async def repmeme(memerep)
+ Rep = await reply_id(memerep)
+ url = f"https://t.me/Repthon_meme/16"
+ await memerep.client.send_file(memerep.chat_id,url,caption="",parse_mode="html",reply_to=Rep)
+ await memerep.delete()
