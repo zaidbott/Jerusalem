08843b93ca503c13304a0bfdffa37692461cafb3
روجر <104784913+rogerpq@users.noreply.github.com>
2023-11-10 07:54:53 +0300

    Update utube.py

--- repthon/helpers/functions/utube.py
+++ repthon/helpers/functions/utube.py
@@ -32,11 +32,11 @@ name_dl = (
 )
 
 
-async def yt_search(zed):
+async def yt_search(rep):
     try:
-        zed = urllib.parse.quote(zed)
+        rep = urllib.parse.quote(rep)
         html = urllib.request.urlopen(
-            f"https://www.youtube.com/results?search_query={zed}"
+            f"https://www.youtube.com/results?search_query={rep}"
         )
 
         user_data = re.findall(r"watch\?v=(\S{11})", html.read().decode())
@@ -89,8 +89,8 @@ class YT_Search_X:
 ytsearch_data = YT_Search_X()
 
 """
-async def yt_data(zed):
-    params = {"format": "json", "url": zed}
+async def yt_data(rep):
+    params = {"format": "json", "url": rep}
     url = "https://www.youtube.com/oembed"  # https://stackoverflow.com/questions/29069444/returning-the-urls-as-a-list-from-a-youtube-search-query
     query_string = urllib.parse.urlencode(params)
     url = f"{url}?{query_string}"
