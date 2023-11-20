a97c901b27f753a5b9e349e02b563bf257720e49
zaidbott <149216928+zaidbott@users.noreply.github.com>
2023-11-20 15:43:51 +0300

    __init__.py

--- repthon/sql_helper/__init__.py
+++ repthon/sql_helper/__init__.py
@@ -1,17 +1,14 @@
 import os
 import subprocess
-
-subprocess.run(["pip", "install", "redis", "beaker"])
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from beaker.cache import CacheManager
 from beaker.util import parse_cache_config_options
 
-# the secret configuration specific things
-from ..Config import Config
-from ..core.logger import logging
+# Import Config and logging based on your project structure
+from path.to.Config import Config
+from path.to.core.logger import logging
 
 LOGS = logging.getLogger(__name__)
 
@@ -25,7 +22,7 @@ cache_opts = {
 
 cache = CacheManager(**parse_cache_config_options(cache_opts))
 
-
+# Function to start the database session
 def start() -> scoped_session:
     redis_url = (
         Config.REDIS_URI.replace("redis:", "redisql:")
@@ -42,7 +39,6 @@ def start() -> scoped_session:
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-
 @cache.cache('my_cached_data', expire=3600)  # Adjust expire time as needed
 def get_data_from_database():
     # Function to retrieve data from the database
@@ -51,12 +47,11 @@ def get_data_from_database():
     data = [...]  # Replace this with your actual data retrieval logic
     return data
 
-
 try:
     BASE = declarative_base()
     SESSION = start()
 except AttributeError as e:
-    # this is a dirty way for the work-around required for #23
+    # This is a dirty way for the work-around required for #23
     LOGS.error(
         "DB_URI is not configured. Features depending on the database might have issues."
     )
