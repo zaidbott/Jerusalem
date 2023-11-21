68ba94082bcabc24330667ae0d7a9eed1cbdd7d9
روجر <104784913+rogerpq@users.noreply.github.com>
2023-11-21 16:14:35 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,15 +1,19 @@
+from typing import set
 from sample_config import Config
 
 
 class Development(Config):
-    # ضـع اكـوادك تحت مكـان الكـلام العـربي  .. امسـح الكلام العربي وضع اكوادك
-    APP_ID = 6
+    # ضـع اكـوادك تحت مكـان الكـلام العـربي  .. امسـح الكلام العربي اللي بين " " فقـط وضع اكـوادك
+    APP_ID = 6 #ضع كود الايبي ايدي الصغيـر مكان الرقم 6 نسخ لصق   
     API_HASH = "ضع كود الايبي هاش"
     ALIVE_NAME = "اسم حسابك التلي"
     DB_URI = "رابـط التخـزين الخـاص بك"
     STRING_SESSION = "كود تيرمــكس"
     TG_BOT_TOKEN = "توكـن البـوت الخـاص بك"
-    PRIVATE_GROUP_BOT_API_ID = -100
-    COMMAND_HAND_LER = "."
-    SUDO_COMMAND_HAND_LER = "،"
-    TZ = "Asia/Baghdad"
+    PM_LOGGER_GROUP_ID = "-100" # ايدي كروب التخزين
+    PRIVATE_GROUP_BOT_API_ID = "-100" # ضـع ايـدي كـروب السجـل بجـانب العـدد -100 لا تمسـح العـدد هـذا
+    COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
+    SUDO_COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
+    TZ = "Asia/Baghdad"  # اتركهــا كمـا هي
+    ENV = "ANYTHING" # اتركـهــا كمـا هي
+    
